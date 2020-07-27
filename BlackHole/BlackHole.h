44423ee740afb85d5f87353235d3219a3ad92872
Nithin-anim <nithin@animaker.com>
2020-07-27 15:49:44 +0530

    Revert latest commit due to code-sign issues

--- BlackHole/BlackHole.h
+++ BlackHole/BlackHole.h
@@ -123,12 +123,12 @@ static pthread_mutex_t              gPlugIn_StateMutex                  = PTHREA
 static UInt32                       gPlugIn_RefCount                    = 0;
 static AudioServerPlugInHostRef     gPlugIn_Host                        = NULL;
 
-#define                             kBox_UID                            "Vmaker_UID"
+#define                             kBox_UID                            "BlackHole_UID"
 static CFStringRef                  gBox_Name                           = NULL;
 static Boolean                      gBox_Acquired                       = true;
 
-#define                             kDevice_UID                         "Vmaker_UID"
-#define                             kDevice_ModelUID                    "Vmaker_ModelUID"
+#define                             kDevice_UID                         "BlackHole_UID"
+#define                             kDevice_ModelUID                    "BlackHole_ModelUID"
 static pthread_mutex_t              gDevice_IOMutex                     = PTHREAD_MUTEX_INITIALIZER;
 static Float64                      gDevice_SampleRate                  = 44100.0;
 static UInt64                       gDevice_IOIsRunning                 = 0;
@@ -152,7 +152,7 @@ static bool                         gMute_Output_Master_Value           = false;
 static const UInt32                 kDataSource_NumberItems             = 1;
 #define                             kDataSource_ItemNamePattern         "BlackHole %ich"
 
-#define                             DEVICE_NAME                         "Vmaker Audio Device"
+#define                             DEVICE_NAME                         "BlackHole %ich"
 #define                             MANUFACTURER_NAME                   "Existential Audio Inc."
 
 static UInt32                       gDataSource_Input_Master_Value      = 0;
